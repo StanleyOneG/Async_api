5a3aead74cce077a2fabd0a03c10085fc66f75de
vlad <vlad@ubuntu.myguest.virtualbox.org>
2023-03-27 22:21:48 +0300

    genres tests added, fixtures moved to conftest

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -9,9 +9,15 @@ import pytest
 from redis import Redis
 from functional.settings import test_settings, test_indexes, index_names
 import logging
-
+from functional.utils.helpers import fake
 
 logger = logging.getLogger('tests')
+
+genres_uuids = [fake.uuid4() for _ in range(10)]
+genres_names = [fake.genre_name() for _ in range(10)]
+
+persons_uuids = [fake.uuid4() for _ in range(10)]
+persons_names = [fake.name() for _ in range(10)]
 
 
 class ElasticIndex(BaseModel):
@@ -23,9 +29,9 @@ class ElasticIndex(BaseModel):
 
 @backoff.on_exception(backoff.expo, (ConnectionError, TransportError))
 async def create_index(
-    es_index: str,
-    es_schema: FilePath,
-    es_client: AsyncElasticsearch,
+        es_index: str,
+        es_schema: FilePath,
+        es_client: AsyncElasticsearch,
 ) -> None:
     """Create Elasticsearch index if it does not exist."""
     with open(es_schema, 'r') as file:
@@ -46,7 +52,7 @@ async def create_index(
 
 
 def get_es_bulk_query(
-    data: list[dict], index: str, id_field: str
+        data: list[dict], index: str, id_field: str
 ) -> list[dict]:
     bulk_query = []
     for row in data:
@@ -76,12 +82,11 @@ def redis_client() -> Generator[Redis, None, None]:
 
 @pytest.fixture(scope='function')
 async def get_client_session():
-    session = aiohttp.ClientSession()
-    yield session
-    session.close()
+    async with aiohttp.ClientSession() as session:
+        yield session
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 async def es_client(request) -> Generator[AsyncElasticsearch, None, None]:
     es_client = AsyncElasticsearch(
         test_settings.es_host,
@@ -115,14 +120,139 @@ def cleanup(request):
 
 @pytest.fixture
 def es_write_data(es_client):
-    async def inner(data: list[dict]):
+    async def inner(data: list[dict], index: str):
         async for client in es_client:
             bulk_query = get_es_bulk_query(
-                data, test_settings.es_movies_index, test_settings.es_id_field
+                data, index, test_settings.es_id_field
             )
             str_query = '\n'.join(bulk_query) + '\n'
             response = await client.bulk(body=str_query, refresh=True)
             if response['errors']:
                 raise Exception('Ошибка записи данных в Elasticsearch')
+
+    return inner
+
+
+@pytest.fixture
+def genres_data() -> list[dict]:
+    es_data = [
+        {
+            'uuid': str(genre_uuid),
+            'name': genre_name,
+        }
+        for genre_uuid, genre_name in zip(genres_uuids, genres_names)
+    ]
+    return es_data
+
+
+@pytest.fixture
+def make_genre_id_request(get_client_session):
+    async def inner():
+        uuid_to_get = genres_uuids[3]
+        url = test_settings.service_url + f'/api/v1/genres/{uuid_to_get}'
+        async for session in get_client_session:
+            response = await session.get(url)
+            return response
+
+    return inner
+
+
+@pytest.fixture
+def make_genres_request(get_client_session):
+    async def inner():
+        url = test_settings.service_url + f'/api/v1/genres/'
+        async for session in get_client_session:
+            response = await session.get(url)
+            return response
+
+    return inner
+
+
+@pytest.fixture
+def persons_data():
+    es_data = [
+        {
+            'uuid': str(person_uuid),
+            'full_name': person_name,
+            'film_work_ids': [],
+        }
+        for person_uuid, person_name in zip(persons_uuids, persons_names)
+    ]
+    es_data.extend(
+        [
+            {
+                'uuid': '56b541ab-4d66-4021-8708-397762bff2d4',
+                'full_name': 'Ivan Ivanov',
+                'film_work_ids': ['b92ef010-5e4c-4fd0-99d6-41b6456272cd'],
+            }
+        ]
+    )
+    return es_data
+
+
+@pytest.fixture
+def film_data():
+    es_data = [
+        {
+            'uuid': 'b92ef010-5e4c-4fd0-99d6-41b6456272cd',
+            'imdb_rating': 8.5,
+            'genre': [
+                {'uuid': str(fake.uuid4()), 'name': 'Sci-Fi'},
+            ],
+            'title': 'Terminator',
+            'description': 'New World',
+            'directors': [{'uuid': '1234', 'full_name': 'John Doe'}],
+            'actors_names': ['Ivan Ivanov', 'Bob'],
+            'writers_names': ['Ben', 'Howard'],
+            'actors': [
+                {
+                    'uuid': '56b541ab-4d66-4021-8708-397762bff2d4',
+                    'full_name': 'Ivan Ivanov',
+                },
+                {'uuid': '222', 'full_name': 'Bob'},
+            ],
+            'writers': [
+                {'uuid': '333', 'full_name': 'Ben'},
+                {'uuid': '444', 'full_name': 'Howard'},
+            ],
+        }
+    ]
+    return es_data
+
+
+@pytest.fixture
+def make_film_get_request(get_client_session):
+    async def inner():
+        uuid_to_get = persons_uuids[3]
+        url = test_settings.service_url + f'/api/v1/persons/{uuid_to_get}'
+        async for session in get_client_session:
+            response = await session.get(url)
+            return response
+
+    return inner
+
+
+@pytest.fixture
+def make_film_request(get_client_session):
+    async def inner():
+        uuid_to_get = '56b541ab-4d66-4021-8708-397762bff2d4'
+        url = test_settings.service_url + f'/api/v1/persons/{uuid_to_get}/film'
+        async for session in get_client_session:
+            response = await session.get(url)
+            return response
+
+    return inner
+
+
+@pytest.fixture
+def make_film_search_request(get_client_session):
+    async def inner(data_to_search):
+        params = {
+            'query': data_to_search,
+        }
+        url = test_settings.service_url + '/api/v1/persons/search'
+        async for session in get_client_session:
+            response = await session.get(url, params=params)
+            return response
 
     return inner
--- tests/functional/src/test_persons.py
+++ tests/functional/src/test_persons.py
@@ -1,6 +1,44 @@
-from faker import Faker
-import uuid
 import logging
-
+import pytest
+from functional.settings import test_settings
+from functional.conftest import persons_uuids, persons_names
 
 logger = logging.getLogger('test_persons')
+
+
+@pytest.mark.asyncio
+async def test_get_person(
+        es_write_data,
+        make_film_get_request,
+        persons_data,
+):
+    await es_write_data(persons_data, test_settings.es_persons_index)
+    response = await make_film_get_request()
+    body = await response.json()
+    status = response.status
+
+    assert status == 200
+    assert body.get('uuid') == persons_uuids[3]
+    assert body.get('full_name') == persons_names[3]
+
+
+@pytest.mark.asyncio
+async def test_get_persons_film(es_write_data, make_film_request, film_data):
+    await es_write_data(film_data, test_settings.es_movies_index)
+    response = await make_film_request()
+    body = await response.json()
+    status = response.status
+
+    assert status == 200
+    assert body[0].get('uuid') == 'b92ef010-5e4c-4fd0-99d6-41b6456272cd'
+    assert body[0].get('title') == 'Terminator'
+
+
+@pytest.mark.asyncio
+async def test_search_person(make_film_search_request):
+    response = await make_film_search_request('Ivan')
+    body = await response.json()
+    status = response.status
+
+    assert status == 200
+    assert body[0].get('full_name') == 'Ivan Ivanov'
